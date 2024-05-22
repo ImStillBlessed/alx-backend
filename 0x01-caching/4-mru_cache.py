7faf16935a6655edd0b75e866381bb0e26fa63b6
Oigbochie Blessed <eprince1800@gmail.com>
2024-05-22 20:02:08 +0100

    task 4 most recently used most cache

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+This module is an implementation of a simple
+MRU most recently used caching system
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    This is a class for a MRU caching system
+    inheriting from BaseCaching
+    """
+    def __init__(self):
+        """
+        Initialization method for the class
+            cache_data: dict {}
+        from the parent class BaseCaching
+        """
+        super().__init__()
+        self.cache_order = []
+
+    def put(self, key, item):
+        """
+        This method adds a new item to the dictionary
+        and removes the most recently used item to make way for new ones.
+        """
+        if key and item:
+            if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+                discard = self.cache_order.pop()
+                del self.cache_data[discard]
+                print(f"DISCARD: {discard}")
+            self.cache_data[key] = item
+            self.cache_order.append(key)
+
+    def get(self, key):
+        """
+        This method returns the value item from
+        the key value pair in the cache_data dict
+        args:
+            key: str
+        returns: the value pair else None
+        """
+        if key in self.cache_data:
+            self.cache_order.remove(key)
+            self.cache_order.append(key)
+        return self.cache_data.get(key)
