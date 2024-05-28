f0a8bb417ef029249e5d9d3e7e908bda508702d6
Oigbochie Blessed <eprince1800@gmail.com>
2024-05-28 13:23:22 +0100

    task 1 complete 0x02-i18n/1-app.py

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,15 +4,17 @@ from flask import Flask, render_template
 from flask_babel import Babel
 
 app = Flask(__name__)
-app.config.from_object('1-app.Config')
-babel = Babel(app)
 
 
 class Config:
     """Config class for the flask app"""
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_LOCALE = "fr"
     BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object('1-app.Config')
+babel = Babel(app)
 
 
 @app.route('/', strict_slashes=False)
@@ -22,4 +24,4 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(debug=True)
